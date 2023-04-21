0c83925a19ac6aa5214c0807b81b8aac4963fa0d
dreamcomes-true <116873401+dreamcomes-true@users.noreply.github.com>
2023-04-21 21:23:03 +0900

    init

---
+++ .github/week2/foodlist.js
@@ -0,0 +1,44 @@
+export const hansik = [
+    {
+      hansik_name: '고등어구이',
+      category: "fish",
+      img_ad: "fish1.jpg",
+      tag: ['겉바속촉', '짱맛']
+    },
+    {
+      hansik_name: '조기구이',
+      category: "fish",
+      img_ad: "fish2.jpg",
+      tag: ['깔끔단백', '밥도둑']
+    },
+    {
+      hansik_name: '갈치구이',
+      category: "fish",
+      img_ad: "fish3.png",
+      tag: ['소금구이', ' 카레']
+    },
+    {
+      hansik_name: '김치찌개',
+      category: "soup",
+      img_ad: "soup1.jpg",
+      tag: ['줄서서', '먹어요']
+    },
+    {
+      hansik_name: '된장찌개',
+      category: "soup",
+      img_ad: "soup2.jpg",
+      tag: ['애호박', ' 청양고추']
+    },
+    {
+      hansik_name: '순두부찌개',
+      category: "soup",
+      img_ad: "soup3.jpg",
+      tag: ['차돌', '라면사리']
+    },
+    {
+      hansik_name: '잔치국수',
+      category: "noodle",
+      img_ad: "noodle.png",
+      tag: ['멸치육수', '굿굿']
+    },
+  ];
