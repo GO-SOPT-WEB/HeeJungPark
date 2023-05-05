5c11c6cefe935d63ffe8c3ae4619f313a16187d9
parkheeddong <phj7214go@gmail.com>
2023-05-05 22:19:09 +0900

    basicAssignment

---
+++ memoryGame/src/MainPage.jsx
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+import HeaderSection from "./Section/HeaderSection";
+import LevelSection from "./Section/LevelSection";
+import GameSection from "./Section/GameSection";
+
+
+const MainPageWrapper = styled.div`
+  background-color: pink;
+  height: 100vh;
+`;
+
+const MainPage = () => {
+
+  const [level, setLevel] = useState("Easy");
+  const [ChosenList, setChosenList] = useState([]);
+  const [AnswerList, setAnswerList] = useState([]);
+  const findingPair = (card) => {
+    setChosenList(ChosenList.concat(card))
+    if ((ChosenList.length === 1) && (ChosenList[0].name === card.name)){
+      setAnswerList(AnswerList.concat({pair0: ChosenList[0], pair1: card}));
+    }
+  };
+  const Reset = () => {
+    setChosenList([]);
+    setAnswerList([]);
+  }
+  return (
+    <>
+      <MainPageWrapper>
+        <HeaderSection
+          chosenLevel={level}
+          AnswerList={AnswerList}
+          Reset={Reset}
+        />
+        <LevelSection
+          chosenLevel={level}
+          setLevel={setLevel}
+        />
+        <GameSection
+          chosenLevel={level}
+          findingPair={findingPair}
+          setChosenList={setChosenList}
+          ChosenList = {ChosenList}
+          AnswerList = {AnswerList}
+        />
+      </MainPageWrapper>
+    </>
+  );
+};
+
+export default MainPage;
+
