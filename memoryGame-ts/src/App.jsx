235a35e5e6c731c5b9a1b752fb4e0964fe9453f0
HeeJungPark <phj7214na@naver.com>
2023-06-09 19:14:47 +0900

    init :초기 세팅

---
+++ memoryGame-ts/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MainPage from "./MainPage";
+import GlobalStyle from "./GlobalStyle";
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <MainPage/>
+    </>
+  );
+}
+
+export default App;
