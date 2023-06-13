b546483a12c6ae066100ec8dcb1b6c31f741e3c4
HeeJungPark <phj7214na@naver.com>
2023-06-14 01:36:10 +0900

    todomate refactoring

---
+++ todomate/data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      category: '솝트',
+      tasks: ['세미나 과제', '스터디 과제']
+    },
+    {
+      category: '학교수업',
+      tasks: ['소공 팀플', '시프 과제']
+    },
+    {
+      category: '루틴',
+      tasks: ['물 2L 마시기', '자전거 타기']
+    },
+    {
+      category: '청소',
+      tasks: ['이불 개기']
+    },
+    {
+      category: '강아지',
+      tasks: ['산책', '배변판 청소']
+    }
+  ];
